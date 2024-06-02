e2c9f29c7e24fdce7c3354278fbd64d462054669
Asad Khan <78179407+Asad-Khan9@users.noreply.github.com>
2024-06-02 19:50:47 +0300

    Update Streamlit_app.py
    added process logs and slight changes

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,136 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# from io import StringIO
-# import time
-# import os
-# import pydeck as pdk
-# import numpy as np
-# from back import toLower, reutrn_columns, remove_punctuation, remove_stopwords, expand_contractions, lemmatize
-
-# st.set_page_config(layout="wide")
-
-
-# import base64
-
-# # Function to get the base64 encoded string of a binary file
-# def get_base64_of_bin_file(bin_file):
-#     with open(bin_file, 'rb') as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
-
-# # Define custom CSS styles
-# styles = """
-# <style>
-# .title-container {
-#     display: flex;
-#     align-items: center;
-#     justify-content: center;
-# }
-
-# .title-image {
-#     margin-right: 10px;
-#     height: 60px;
-# }
-
-# .title-text {
-#     font-size: 3rem;
-#     font-weight: bold;
-# }
-# </style>
-# """
-
-# # Apply the custom CSS styles
-# st.markdown(styles, unsafe_allow_html=True)
-
-# # Render the title with an image
-# title_html = """
-# <div class="title-container">
-#     <img class="title-image" src="data:image/png;base64,{}" alt="Title Image">
-#     <span class="title-text">NLP Data Preprocessing Tool</span>
-# </div>
-# """.format(get_base64_of_bin_file("titleimage\\table.png"))
-
-# st.markdown(title_html, unsafe_allow_html=True)
-# # # st.title("<center>NLP Data Preprocessing</center>", unsafe_allow_html = True)
-# # st.header("<center>Centered Header</center>", unsafe_allow_html=True)
-
-# # st.write("Prepare your dataset here for machine learning")
-
-# st.markdown("<center>Perform end-to-end data preprocessing for your NLP projects</center>", unsafe_allow_html=True)
-# st.divider()
-# st.write("Upload  the file here")  
-# uploaded_file = st.file_uploader("200MB")
-# st.divider()  
-# if uploaded_file is not None:
-#     df1 = pd.read_csv(uploaded_file)
-#     # df2 = df1
-#     st.header("Initial Dataframe")
-#     st.write(df1)
-
-#     file_path = os.path.join("dataset\\", uploaded_file.name)
-#     with open(file_path, "wb") as f:
-#         f.write(uploaded_file.getbuffer())
-    
-#     columns_ = df1.columns
-#     columns = {"None"}.union(columns_)
-#     num_rows = df1.shape[0]
-    
-#     st.divider()
-#     st.subheader("Text Cleaning")
-
-#     col1, col2 = st.columns([0.32, 0.68])
-#     with col1:
-#         tab1, tab2, tab3 = st.tabs(["Lowercasing", "Remove Punctuation", "Remove Stopwords"])
-#         with tab1:
-#             st.subheader("Preprocessing Methods")
-#             # column_ = st.selectbox("select your column to preprocess", set(columns), index=0)
-#             Lowercase_columns = st.multiselect('Select the columns to be lowercased',columns_)
-               
-#             for i in Lowercase_columns: 
-#                 with st.spinner(f'preprocessing column {i}'):
-#                     time.sleep(3) 
-#                     toLower(df1, i)
-#             # toLower(df1, column_)
-#         st.divider()
-#         with tab2:
-#             Remove_punctuation_columns = st.multiselect('Select the columns to remove punctuations from',columns_)
-#             # st.write('After removing punctuation from "{}" column'.format("last_sold_agency"))
-#             for i in Remove_punctuation_columns:
-#                 df1[i] = df1[i].apply(remove_punctuation)
-
-#         with tab3:
-#             stopword_columns = st.multiselect('select columns to remove stopwords from', columns_)
-
-#             for i in stopword_columns:  
-#                 df1[i] = df1[i].apply(remove_stopwords)
-            
-#         st.subheader("Stemming/Lemmatization")
-#         tab4, tab5, tab6 = st.tabs(["Contractions", "Lemmatization", "Stemming"])
-
-#         with tab4: 
-#             st.subheader("Contractions")
-#             contractions_ = st.multiselect('Select the columns for contractions',columns_)
-#             for i in contractions_:
-#                 df1[i] = df1[i].apply(expand_contractions)
-#         with tab5:
-#             st.subheader("Lemmatization")
-#             lemmatize_ = st.multiselect('Select the columns for lemmatization',columns_)
-#             for i in lemmatize_:
-#                 df1[i] = df1[i].apply(lemmatize)
-#         with tab6:
-                
-#             st.subheader("eryu")
-
-
-#         # st.markdown("*Streamlit* is **really** ***cool***.")
-#         st.divider()  
-#     with col2:
-#         # st.success("Initial Dataframe")
-#         # st.write(df2)
-#         st.header("Processed Dataframe")
-#         st.write(df1)
-
-#-----------------------------------------------------------------------------------------------------------------
 import streamlit as st
 import pandas as pd
 import time
@@ -139,7 +6,8 @@ from back import toLower, removePunctuation, removeStopwords, expandContractions
 
 st.set_page_config(layout="wide")
 
-import base64
+import base64                             
+
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
@@ -195,31 +63,33 @@ if uploaded_file is not None:
     columns_ = df1.columns
     columns = {"None"}.union(columns_)
     num_rows = df1.shape[0]
-    
+    multiprocessFlag = 1 if num_rows > 25000 else 0
     st.divider()
+    st.header("Preprocessing Methods")
     st.subheader("Text Cleaning")
-
+    processLog = []
     col1, col2 = st.columns([0.32, 0.68])
     with col1:
         tab1, tab2, tab3 = st.tabs(["Lowercasing", "Remove Punctuation", "Remove Stopwords"])
         with tab1:
-            st.subheader("Preprocessing Methods")
             Lowercase_columns = st.multiselect('Select the columns to be lowercased', columns_)
             for i in Lowercase_columns:
                 with st.spinner(f'Processing column {i}'):
-                    df1 = toLower(df1, i)
-        st.divider()
+                    df1 = toLower(df1, i, multiprocessFlag)
+                    processLog.append(f'Processed column "{i}": lowercased')
+        # st.divider()
         with tab2:
             Remove_punctuation_columns = st.multiselect('Select the columns to remove punctuations from', columns_)
             for i in Remove_punctuation_columns:
                 with st.spinner(f'Processing column {i}'):
-                    df1 = removePunctuation(df1, i)
+                    df1 = removePunctuation(df1, i, multiprocessFlag)
+                    processLog.append(f'Processed column "{i}": removed punctuations')
         with tab3:
             stopword_columns = st.multiselect('Select columns to remove stopwords from', columns_)
             for i in stopword_columns:
                 with st.spinner(f'Processing column {i}'):
-                    df1 = removeStopwords(df1, i)
-
+                    df1 = removeStopwords(df1, i, multiprocessFlag)
+                    processLog.append(f'Processed column "{i}": removed stopwords')
         st.subheader("Stemming/Lemmatization")
         tab4, tab5 = st.tabs(["Contractions", "Lemmatization"])
         with tab4: 
@@ -227,15 +97,17 @@ if uploaded_file is not None:
             contractions_ = st.multiselect('Select the columns for contractions', columns_)
             for i in contractions_:
                 with st.spinner(f'Processing column {i}'):
-                    df1 = expandContractions(df1, i)
+                    df1 = expandContractions(df1, i, multiprocessFlag)
+                    processLog.append(f'Processed column "{i}": expanded contractions')
         with tab5:
             st.subheader("Lemmatization")
             lemmatize_ = st.multiselect('Select the columns for lemmatization', columns_)
             for i in lemmatize_:
                 with st.spinner(f'Processing column {i}'):
-                    df1 = lemmatizeText(df1, i)
+                    df1 = lemmatizeText(df1, i, multiprocessFlag)
+                    processLog.append(f'Processed column "{i}": lemmatized')
 
-        st.divider()  
+        # st.divider()  
     with col2:
         st.header("Processed Dataframe")
         st.write(df1)
@@ -243,11 +115,10 @@ if uploaded_file is not None:
             return df.to_csv(index=False).encode('utf-8')
         csv = convert_df(df1)
 
-        st.download_button(
-           "Download Processed Data Frame",
-           csv,
-           "file.csv",
-           "text/csv",
-           key='download-csv'
-        )
-    
+        st.download_button("Download Processed Dataframe",csv,"file.csv","text/csv",key='download-csv')
+
+    st.divider()
+    st.header("Log")
+    for i in processLog:
+        st.code(i)
+    # st.code(processLog)
