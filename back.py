0639ee9402fa94b92c0ac636ccdf35fb77723824
Asad Khan <78179407+Asad-Khan9@users.noreply.github.com>
2024-06-02 19:53:42 +0300

    Update back.py

--- back.py
+++ back.py
@@ -1,72 +1,56 @@
-# import pandas as pd
-# import numpy as np
-# import nltk
-# import string
-# import contractions as contr
-# from nltk.stem import WordNetLemmatizer
-
-# def reutrn_columns(df):
-#     return(df.columns)
-
-# def toLower(df, column):
-#     df[column] = df[column].str.lower()
-#     return df
-
-# def remove_punctuation(text):
-#     if isinstance(text, str):
-#         tokenizer = nltk.RegexpTokenizer(r'\w+')
-#         tokens = tokenizer.tokenize(text)
-#         return ' '.join(tokens)
-#     else:
-#         return text
-    
-# def remove_stopwords(text):
-#     if isinstance(text, str):
-#         tokens = nltk.word_tokenize(text)
-#         stopwords = nltk.corpus.stopwords.words("english")
-#         filtered = [token for token in tokens if token.lower() not in stopwords]
-#         return ' '.join(filtered)
-
-# def expand_contractions(text):
-#     if isinstance(text, str):
-#         expanded_words = [] 
-#         for word in text.split():
-#             expanded_words.append(contr.fix(word))  
-        
-#         return ' '.join(expanded_words)
-
-# # def lemmatize(text):
-# #     wnl = WordNetLemmatizer()
-# #     if isinstance(text, str):
-# #         text_tokens = nltk.word_tokenize(text)
-# #         return " ".join(wnl.lemmatize(text_tokens, pos="v"))
-    
-# # =========================================
-# # nltk.download("wordnet")
-# # nltk.download("omw-1.4")
-# # =========================================
-    
-# def lemmatize(text):
-#     wnl = WordNetLemmatizer()
-#     if isinstance(text, str):
-#         text_tokens = nltk.word_tokenize(text)
-#         lemmatized_tokens = [wnl.lemmatize(word, pos="v") for word in text_tokens]
-#         return ' '.join(lemmatized_tokens)
-
-#--------------------------------------------------------------------------------------
-
 import pandas as pd
 import nltk
 import contractions as contr
 from nltk.stem import WordNetLemmatizer
-from multiprocessing import Pool
 
 # nltk.download('stopwords')
 # nltk.download('punkt')
 # nltk.download("wordnet")
 # nltk.download("omw-1.4")
 
+#---------sequesntial processing-----------
+def reutrn_columns(df):
+    return(df.columns)
 
+def toLower0(df, column):
+    df[column] = df[column].str.lower()
+    return df
+
+def remove_punctuation(text):
+    if isinstance(text, str):
+        tokenizer = nltk.RegexpTokenizer(r'\w+')
+        tokens = tokenizer.tokenize(text)
+        return ' '.join(tokens)
+    else:
+        return text
+    
+def remove_stopwords(text):
+
+    if isinstance(text, str):
+        
+        stopwords_set = set(nltk.corpus.stopwords.words("english"))
+        tokens = nltk.word_tokenize(text)
+        stopwords = nltk.corpus.stopwords.words("english")
+        filtered = [token for token in tokens if token.lower() not in stopwords]
+        return ' '.join(filtered)
+
+def expand_contractions(text):
+    if isinstance(text, str):
+        expanded_words = [] 
+        for word in text.split():
+            expanded_words.append(contr.fix(word))  
+        
+        return ' '.join(expanded_words)
+
+def lemmatize(text):
+    wnl = WordNetLemmatizer()
+    if isinstance(text, str):
+        text_tokens = nltk.word_tokenize(text)
+        lemmatized_tokens = [wnl.lemmatize(word, pos="v") for word in text_tokens]
+        return ' '.join(lemmatized_tokens)
+
+
+#---------------------multiprocessing----------------
 def to_lower(text):
     if isinstance(text, str):
         return text.lower()
@@ -99,6 +83,8 @@ def lemmatize(text):
         lemmatized_tokens = [wnl.lemmatize(word, pos="v") for word in text_tokens]
         return ' '.join(lemmatized_tokens)
     return text
+#-----------multiprocessing--------------------
+from multiprocessing import Pool
 
 def process_chunk(df_chunk, func, column, *args):
     df_chunk[column] = df_chunk[column].apply(lambda x: func(x, *args))
@@ -115,18 +101,38 @@ def parallelize_dataframe(df, func, column, *args, num_partitions=16):
     pool.join()
     return pd.concat(processed_chunks)
 
-def toLower(df, column):
-    return parallelize_dataframe(df, to_lower, column)
+def toLower(df, column, multiprocessFlag):
+    if multiprocessFlag == 1:
+        return parallelize_dataframe(df, to_lower, column)
+    return toLower0(df, column)
 
-def removePunctuation(df, column):
-    return parallelize_dataframe(df, remove_punctuation, column)
+def removePunctuation(df, column, multiprocessFlag):
+    if multiprocessFlag == 1:
+        processed_df = parallelize_dataframe(df, remove_punctuation, column)
+        return processed_df
+    df[column] = df[column].apply(remove_punctuation)
+    return df
 
-def removeStopwords(df, column):
-    stopwords_set = set(nltk.corpus.stopwords.words("english"))
-    return parallelize_dataframe(df, remove_stopwords_from_text, column, stopwords_set)
+def removeStopwords(df, column, multiprocessFlag):
+    if multiprocessFlag == 1:
+        stopwords_set = set(nltk.corpus.stopwords.words("english"))
+        processed_df = parallelize_dataframe(df, remove_stopwords_from_text, column, stopwords_set)
+        return processed_df
+    df[column] = df[column].apply(lambda text: remove_stopwords_from_text(text, set(nltk.corpus.stopwords.words("english"))))
+    return df
 
-def expandContractions(df, column):
-    return parallelize_dataframe(df, expand_contractions, column)
 
-def lemmatizeText(df, column):
-    return parallelize_dataframe(df, lemmatize, column)
+def expandContractions(df, column, multiprocessFlag):
+    if multiprocessFlag == 1:
+        processed_df = parallelize_dataframe(df, expand_contractions, column)
+        return processed_df
+    df[column] = df[column].apply(expand_contractions)
+    return df
+
+
+def lemmatizeText(df, column, multiprocessFlag):
+    if multiprocessFlag == 1:
+        processed_df = parallelize_dataframe(df, lemmatize, column)
+        return processed_df
+    df[column] = df[column].apply(lemmatize)
+    return df
